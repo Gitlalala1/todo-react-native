78b37d9aea9682f9c50bcaaa45ddaf899706a206
Gitlalala1 <gitbumlala@mail.ru>
2023-03-13 17:24:14 +0300

    Подключение шрифтов. Асинхронное подключение (устаревший)

--- App.js
+++ App.js
@@ -1,14 +1,35 @@
 import { StyleSheet, Text, View, Alert } from "react-native";
+import * as Font from "expo-font";
 import React, { useState } from "react";
+import AppLoading from "expo-app-loading";
+
 import Navbar from "./src/components/navbar";
 import MainScreen from "./src/screens/main-screen";
 import TodoScreen from "./src/screens/todo-screen";
+
+async function loadApplication() {
+	await Font.loadAsync({
+		"roboto-regular": require("./assets/fonts/Roboto-Regular.ttf"),
+		"roboto-bold": require("./assets/fonts/Roboto-Bold.ttf"),
+	});
+}
+
 export default function App() {
+	const [isReady, setIsReady] = useState(false);
 	const [todoId, setTodoId] = useState(null);
 	const [todos, setTodos] = useState([
-		// { id: "1", title: "Drink cofee" },
-		// { id: "2", title: "Drink beer" },
+		{ id: "1", title: "Drink cofee" },
+		{ id: "2", title: "Drink beer" },
 	]);
+	if (!isReady)
+		return (
+			<AppLoading
+				startAsync={loadApplication}
+				onFinish={() => setIsReady(true)}
+				onError={(err) => console.log(err)}
+			/>
+		);
+
 	const addTodo = (title) => {
 		const newTodo = {
 			id: Date.now().toString(),
